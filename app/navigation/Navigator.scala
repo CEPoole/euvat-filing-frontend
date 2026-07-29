750904bf48469c9037a80999c854cf55307a64e7
desi-arabadzhieva <280463992+desi-arabadzhieva@users.noreply.github.com>
2026-07-29 12:55:48 +0100

    DTR-6982 collapse invoice type match

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -197,11 +197,7 @@ class Navigator @Inject() (configCurrencyMapping: ConfigCurrencyMapping, configL
 
   private def navigateFromSimplifiedInvoiceVatRegCheckPage(mode: Mode)(userAnswers: UserAnswers): Call =
     userAnswers.get(SimplifiedInvoiceVatRegCheckPage) match {
-      case Some(true) =>
-        userAnswers.get(InvoiceTypePage) match {
-          case Some(InvoiceType.SimplifiedInvoice) => routes.SupplierVatRegistrationNumberController.onPageLoad(mode)
-          case _                                   => navigateToCurrencyOrPurchaseAmount(mode)(userAnswers)
-        }
+      case Some(true)  => routes.SupplierVatRegistrationNumberController.onPageLoad(mode)
       case Some(false) => navigateToCurrencyOrPurchaseAmount(mode)(userAnswers)
       case _           => routes.JourneyRecoveryController.onPageLoad()
     }
