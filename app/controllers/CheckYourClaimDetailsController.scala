931c895be1c9c23562efbff6ea67f4a1fc05513a
shridhar-chippa <189346280+shridhar-chippa@users.noreply.github.com>
2026-08-20 12:49:15 +0100

    Refactor code

--- app/controllers/CheckYourClaimDetailsController.scala
+++ app/controllers/CheckYourClaimDetailsController.scala
@@ -74,36 +74,41 @@ class CheckYourClaimDetailsController @Inject() (
         }
       }
 
-      val latestReq = LatestApplicationRequest(
-        applicantVatRegNumber = request.identifierValue.getOrElse(throw new IllegalStateException("Missing Vat registration number")),
-        refundingCountry      = userAnswers.get(RefundingCountryPage)
-      )
-
       updatedAnswers
         .flatMap { flaggedAnswers =>
-          service
-            .getLatestApplications(latestReq)
-            .flatMap { latestResp =>
-              if (latestResp.totalApplication > 0) {
-                logger.warn("You cannot have more than one draft claim for each EU member state")
-                Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
-              } else {
-                val claimRequest = buildClaimRequest(flaggedAnswers)
-                saveClaimResponseAndRedirect(flaggedAnswers, claimRequest)
-              }
-            }
-            .recover { case ex =>
-              logger.error("Error while retrieving or saving the refund application", ex)
-              Redirect(routes.JourneyRecoveryController.onPageLoad())
-            }
+          request.identifierValue match {
+            case None =>
+              logger.error("Missing VAT registration number")
+              Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+            case Some(vatRegNumber) =>
+              val latestReq = LatestApplicationRequest(
+                applicantVatRegNumber = vatRegNumber,
+                refundingCountry      = flaggedAnswers.get(RefundingCountryPage)
+              )
+              service
+                .getLatestApplications(latestReq)
+                .flatMap { latestResp =>
+                  if (latestResp.totalApplication > 0) {
+                    logger.warn("You cannot have more than one draft claim for each EU member state")
+                    Future.successful(Redirect(routes.JourneyRecoveryController.onPageLoad()))
+                  } else {
+                    saveClaimResponseAndRedirect(flaggedAnswers, buildClaimRequest(flaggedAnswers))
+                  }
+                }
+          }
         }
+        .recover { case ex =>
+          logger.error("Error while retrieving or saving the refund application", ex)
+          Redirect(routes.JourneyRecoveryController.onPageLoad())
+        }
+
     }
   }
 
-  private def saveClaimResponseAndRedirect(flaggedAnswers: UserAnswers, appRequest: ApplicationRequest)(using RequestHeader): Future[Result] = {
+  private def saveClaimResponseAndRedirect(userAnswers: UserAnswers, appRequest: ApplicationRequest)(using RequestHeader): Future[Result] = {
     for {
       claimResponse  <- service.createApplication(appRequest)
-      updatedAnswers <- Future.fromTry(flaggedAnswers.set(ClaimApplicationResponseQuery, claimResponse))
+      updatedAnswers <- Future.fromTry(userAnswers.set(ClaimApplicationResponseQuery, claimResponse))
       _              <- sessionRepository.set(updatedAnswers)
     } yield {
       if (claimResponse.applicationId > 0) {
